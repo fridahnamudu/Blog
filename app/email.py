53b8b302bad401a681c0f3225300c65b5f4fac7c
fridahnamudu <fridah.namudu@gmail.com>
2020-10-01 22:34:28 +0300

    add changes

--- app/email.py
+++ app/email.py
@@ -4,7 +4,7 @@ from . import mail
 subject_pref = 'New Blog'
 
 def mail_message(subject, template, to, **kwargs):
-    sender_email = 'eugenekiprotich0@gmail.com'
+    sender_email = 'fridah.namudu@gmail.com'
 
     email = Message(subject, sender = sender_email, recipients = [to])
     email.body = render_template(template + ".txt", **kwargs)
